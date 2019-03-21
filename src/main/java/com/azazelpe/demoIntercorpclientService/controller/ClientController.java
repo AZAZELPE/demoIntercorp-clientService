8f7cdea79fff671c1f6bf598a54326851dd97add
Luis Ruiz <lerc2990@gmail.com>
2019-03-21 16:46:49 -0500

    fix controller and added configuration of apigateway

--- src/main/java/com/azazelpe/demoIntercorpclientService/controller/ClientController.java
+++ src/main/java/com/azazelpe/demoIntercorpclientService/controller/ClientController.java
@@ -10,7 +10,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping()
 public class ClientController {
 
     @Autowired
--- src/main/java/com/azazelpe/demoIntercorpclientService/service/ClientService.java
+++ src/main/java/com/azazelpe/demoIntercorpclientService/service/ClientService.java
@@ -5,6 +5,7 @@ import com.azazelpe.demoIntercorpclientService.model.UpdateKpi;
 import com.azazelpe.demoIntercorpclientService.repository.ClientRepository;
 import org.hibernate.sql.Update;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,11 +18,16 @@ public class ClientService {
     @Autowired
     ClientRepository clientRepository;
 
+    @Value("${apigateway.path}")
+    private String apigatewayPath;
+
     public void postUpdateKpis(){
-        final String uri = "http://localhost:8080/api/updateKpi";
+        final String uri = apigatewayPath + "/kpi/updateKpi";
+
         RestTemplate restTemplate = new RestTemplate();
 
         List<Client> clients = clientRepository.findAll();
+
         UpdateKpi updateKpi = new UpdateKpi();
         List<Integer> edades = new ArrayList<>();
         updateKpi.setEdades(edades);
